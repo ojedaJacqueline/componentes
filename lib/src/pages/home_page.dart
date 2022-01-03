ae6e448982e82f0878d922437a7387db4cff4baf
Analia <pasantetelco.analia@gmail.com>
2022-01-03 07:51:03 -0300

    flutter

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,74 @@
+import 'package:componentes/src/pages/utils/icono_string_util.dart';
+import 'package:flutter/material.dart';
+
+import 'package:componentes/src/pages/providers/menu_provider.dart';
+
+
+
+class HomePage  extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( 
+        title:Text('Componentes'),
+      ),
+      body: _lista(),
+    );
+  }
+
+   Widget _lista() {
+    
+     //menuProvider.cargarData()
+
+    return FutureBuilder(
+         future: menuProvider.cargarData(),
+        // initialData:[],
+         builder:( context, AsyncSnapshot <List <dynamic>> snapshot ){
+     
+ 
+
+return ListView(
+  children: _listaItems(snapshot.data,context),
+);
+
+        },
+);
+
+    //  return ListView(
+    //     children: _listaItems(),
+    //  );
+
+   }
+
+  List <Widget> _listaItems(List <dynamic> data , BuildContext contex) {
+
+    final List<Widget> opciones = [];
+
+    data.forEach( (opt) {
+     
+     final widgetTemp = ListTile(
+       title: Text( opt['texto'] ),
+       leading: getIcon(opt['icon']),
+       trailing: Icon(Icons.keyboard_arrow_right,color: Colors.blue),
+       onTap:(){
+         
+
+         final router = MaterialPageRoute(builder: (context) {
+          return AlertPage();
+       }
+         );
+            Navigator.push(context, route);
+        },
+    );
+opciones..add(widgetTemp)
+        ..add(Divider());
+        
+});
+
+    return opciones;
+
+  }
+}
+
